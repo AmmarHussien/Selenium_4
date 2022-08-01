38fdd47416ab2ac8114ff1e78711a492d727fccf
ammar <ammar.hussienmo97@gmail.com>
2022-08-01 14:00:50 +0200

    Sel 4

---
+++ src/main/java/Position.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Position {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testautomationu.applitools.com/learningpaths.html");
+    }
+
+    @Test
+    public void getPositionDimension(){
+        WebElement logoTAU = driver.findElement(
+                By.xpath("//div[@id='app']//header/a/img"));
+        Rectangle rectTAULogo = logoTAU.getRect();
+        System.out.println("x: " + rectTAULogo.getX());
+        System.out.println("y: " + rectTAULogo.getY());
+        System.out.println("height: " + rectTAULogo.getHeight());
+        System.out.println("width: " + rectTAULogo.getWidth());
+
+    }
+}
